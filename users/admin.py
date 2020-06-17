c88bc8b47bc06329bf2968246c718429a2829976
tonky0110 <tonky0110@tonky0110ui-MacBookPro.local>
2020-06-17 23:04:10 +0900

    8.6.2 RoomInline in User Admin Panel

--- users/admin.py
+++ users/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from . import models
+from rooms import models as room_models
+
+
+class RoomInline(admin.TabularInline):
+    model = room_models.Room
 
 
 @admin.register(models.User)
 class CustomUserAdmin(UserAdmin):
 
     """ Custom User Admin """
+
+    inlines = (RoomInline,)
 
     fieldsets = UserAdmin.fieldsets + (
         (
