09baa87d4687eb658d31018a4a4eac706a7eb40d
tonky0110 <tonky0110@tonky0110ui-MacBookPro.local>
2020-06-20 21:25:02 +0900

    9.5 n:m관계의 fake data생성(room)

--- rooms/management/commands/seed_rooms.py
+++ rooms/management/commands/seed_rooms.py
@@ -23,6 +23,9 @@ class Command(BaseCommand):
         seeder = Seed.seeder()
         all_users = user_models.User.objects.all()
         room_types = room_models.RoomType.objects.all()
+        amenities = room_models.Amenity.objects.all()
+        facilities = room_models.Facility.objects.all()
+        rules = room_models.HouseRule.objects.all()
         seeder.add_entity(
             room_models.Room,
             number,
@@ -42,10 +45,24 @@ class Command(BaseCommand):
         created_clean = flatten(list(created_photos.values()))
         for pk in created_clean:
             room = room_models.Room.objects.get(pk=pk)
-            for i in range(3, random.randint(10, 17)):
+            for i in range(3, random.randint(10, 30)):
                 room_models.Photo.objects.create(
                     caption=seeder.faker.sentence(),
                     room=room,
                     file=f"room_photos/{random.randint(1, 31)}.webp",
                 )
+
+            for a in amenities:
+                magic_number = random.randint(0, 15)
+                if magic_number % 2 == 0:
+                    room.amenities.add(a)
+            for f in facilities:
+                magic_number = random.randint(0, 15)
+                if magic_number % 2 == 0:
+                    room.facilities.add(f)
+            for r in rules:
+                magic_number = random.randint(0, 15)
+                if magic_number % 2 == 0:
+                    room.house_rules.add(r)
+
         self.stdout.write(self.style.SUCCESS(f"{number} room(s) created."))
