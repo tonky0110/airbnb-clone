2a37fa34c4ec5070b67a8d954dd1a6eea3ad26ed
tonky0110 <tonky0110@tonky0110ui-MacBookPro.local>
2020-06-23 21:44:07 +0900

    11.6 try except처리 except EmpltyPage: 해당하는 페이지가 없으면... except Exception: 해당하는view가에서 exception이 발생하면...

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,5 @@
-from math import ceil
-from datetime import datetime
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage
 from . import models
 
 
@@ -9,8 +7,12 @@ def all_rooms(request):
     page = request.GET.get("page", 1)
     room_list = models.Room.objects.all()
     paginator = Paginator(room_list, 10, orphans=5)
-    # rooms = paginator.get_page(page)
-    rooms = paginator.page(int(page))
-    # print(dir(rooms))
-    return render(request, "rooms/home.html", {"page": rooms},)
+    try:
+        # rooms = paginator.get_page(page)
+        rooms = paginator.page(int(page))
+        return render(request, "rooms/home.html", {"page": rooms},)
+        # print(dir(rooms))
+    except EmptyPage:
+        # except Exception:
+        return redirect("/")
 
