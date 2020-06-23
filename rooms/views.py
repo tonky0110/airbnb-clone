826e449cef46699d61317844309d23118be481e0
tonky0110 <tonky0110@tonky0110ui-MacBookPro.local>
2020-06-23 22:27:11 +0900

    11.8 function based view vs. class baed view 비교 및 장단점 & get_context_data를 통한 return value수정정

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,4 @@
+# from django.utils import timezone
 from django.views.generic import ListView
 from . import models
 
@@ -10,3 +11,11 @@ class HomeView(ListView):
     paginate_by = 10  # 페이지 사이즈
     paginate_orphans = 5  # 맨 마지막 페이지의 개수가 n개보다 작으면 전페이지에서 모두 조회
     ordering = "created"  # 정렬순
+    context_object_name = "rooms"
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     # context = {}
+    #     now = timezone.now()
+    #     context["now"] = now
+    #     return context
