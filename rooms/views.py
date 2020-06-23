78ae8b2bbbe13d6e1c6cae408adabcbcb5a8d122
tonky0110 <tonky0110@tonky0110ui-MacBookPro.local>
2020-06-23 22:14:04 +0900

    11.7 class based Views ListView를 import해서 view를 구현현 model, paginate_at, parinated_orphans, ordering등을 설정할 수 있고, template를 수정해야한다.

--- rooms/views.py
+++ rooms/views.py
@@ -1,18 +1,12 @@
-from django.shortcuts import render, redirect
-from django.core.paginator import Paginator, EmptyPage
+from django.views.generic import ListView
 from . import models
 
 
-def all_rooms(request):
-    page = request.GET.get("page", 1)
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10, orphans=5)
-    try:
-        # rooms = paginator.get_page(page)
-        rooms = paginator.page(int(page))
-        return render(request, "rooms/home.html", {"page": rooms},)
-        # print(dir(rooms))
-    except EmptyPage:
-        # except Exception:
-        return redirect("/")
+class HomeView(ListView):
 
+    """ HomeView Definition """
+
+    model = models.Room  # 모델
+    paginate_by = 10  # 페이지 사이즈
+    paginate_orphans = 5  # 맨 마지막 페이지의 개수가 n개보다 작으면 전페이지에서 모두 조회
+    ordering = "created"  # 정렬순
