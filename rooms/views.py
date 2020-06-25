185681047bdea431ab127d0e2fea42709b0de093
tonky0110 <tonky0110@tonky0110ui-MacBookPro.local>
2020-06-25 12:43:24 +0900

    13.3 checkbox render 및 파라메터로 전달.

--- rooms/views.py
+++ rooms/views.py
@@ -49,21 +49,41 @@ class RoomDetail(DetailView):
 
 
 def search(request):
-    print(request.GET)
+    # print(request.GET)
     city = request.GET.get("city", "Anywhere")
     city = str.capitalize(city)
     country = request.GET.get("country", "KR")
     room_type = int(request.GET.get("room_type", 0))
-    room_types = models.RoomType.objects.all()
-
+    price = int(request.GET.get("price", 0))
+    guests = int(request.GET.get("guests", 0))
+    bedrooms = int(request.GET.get("bedrooms", 0))
+    beds = int(request.GET.get("beds", 0))
+    baths = int(request.GET.get("baths", 0))
+    s_amenities = request.GET.get("amenities")
+    s_facilities = request.GET.get("facilities")
+    print(s_amenities, s_facilities)
     form = {
         "city": city,
         "s_room_type": room_type,
         "s_country": country,
+        "price": price,
+        "guests": guests,
+        "bedrooms": bedrooms,
+        "beds": beds,
+        "baths": baths,
+        "s_amenities": s_amenities,
+        "s_facilities": s_facilities,
     }
+
+    room_types = models.RoomType.objects.all()
+    amenities = models.Amenity.objects.all()
+    facilities = models.Facility.objects.all()
+
     choices = {
         "countries": countries,
         "room_types": room_types,
+        "amenities": amenities,
+        "facilities": facilities,
     }
     # print(form)
     return render(request, "rooms/search.html", {**form, **choices},)
