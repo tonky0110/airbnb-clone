8b9d3fdb1b6a27555294e8dbef7028723e70f27f
tonky0110 <tonky0110@tonky0110ui-MacBookPro.local>
2020-06-25 14:35:48 +0900

    13.6 filter_args설정. s_amenities, s_facilities는 가장 마지막 값만 비교를 하는것 같아서 수정이 필요할 듯

--- rooms/views.py
+++ rooms/views.py
@@ -61,9 +61,10 @@ def search(request):
     baths = int(request.GET.get("baths", 0))
     s_amenities = request.GET.getlist("amenities")
     s_facilities = request.GET.getlist("facilities")
-    instant = request.GET.get("instant", False)
-    super_host = request.GET.get("super_host", False)
+    instant = bool(request.GET.get("instant", False))
+    superhost = bool(request.GET.get("superhost", False))
 
+    print(f"instant: {instant}, superhost: {superhost}")
     # print(s_amenities, s_facilities)
     form = {
         "city": city,
@@ -77,7 +78,7 @@ def search(request):
         "s_amenities": s_amenities,
         "s_facilities": s_facilities,
         "instant": instant,
-        "super_host": super_host,
+        "super_host": superhost,
     }
 
     room_types = models.RoomType.objects.all()
@@ -100,6 +101,35 @@ def search(request):
 
     if room_type != 0:
         filter_args["room_type__pk"] = room_type
+
+    if price != 0:
+        filter_args["price__gte"] = price
+
+    if guests != 0:
+        filter_args["guests__gte"] = guests
+
+    if bedrooms != 0:
+        filter_args["bedrooms__gte"] = bedrooms
+
+    if beds != 0:
+        filter_args["beds__gte"] = beds
+
+    if baths != 0:
+        filter_args["baths__gte"] = baths
+
+    if instant is True:
+        filter_args["instant_book"] = True
+
+    if superhost is True:
+        filter_args["host__superhost"] = True
+
+    if len(s_amenities) > 0:
+        for s_amenity in s_amenities:
+            filter_args["amenities__pk"] = int(s_amenity)
+
+    if len(s_facilities) > 0:
+        for s_fcility in s_facilities:
+            filter_args["facilities__pk"] = int(s_fcility)
 
     print(filter_args)
 
